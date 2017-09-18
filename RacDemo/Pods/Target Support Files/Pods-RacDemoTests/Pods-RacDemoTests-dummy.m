0f3a56abd6d482b9a98cb61280256fd9f9d23457
alanzhangg <alanzhangg@icloud.com>
2017-09-18 10:09:54 +0800

    add pod

---
+++ RacDemo/Pods/Target Support Files/Pods-RacDemoTests/Pods-RacDemoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RacDemoTests : NSObject
+@end
+@implementation PodsDummy_Pods_RacDemoTests
+@end
---
+++ RacDemo/Pods/Target Support Files/Pods-RacDemoUITests/Pods-RacDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RacDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RacDemoUITests
+@end
